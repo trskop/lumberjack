c9862943f1e7c0b7bb7c40de16dc28296f91d33b
Peter Trško <peter.trsko@gmail.com>
2016-01-09 14:18:40 +0100

    Created separate package for LogLevel data types
    Syslog inspired LogLevel data type moved in to this new package.

--- log-level/src/Data/LogLevel/Syslog.hs
+++ log-level/src/Data/LogLevel/Syslog.hs
@@ -7,7 +7,7 @@
 -- |
 -- Module:       $HEADER$
 -- Description:  TODO
--- Copyright:    (c) 2015, Peter Trško
+-- Copyright:    (c) 2015-2016, Peter Trško
 -- License:      BSD3
 --
 -- Stability:    experimental
@@ -15,7 +15,7 @@
 --               LambdaCase, NoImplicitPrelude, TemplateHaskell
 --
 -- TODO
-module System.Lumberjack.LogLevel
+module Data.LogLevel.Syslog
   where
 
 import Data.Data (Data, Typeable)
@@ -27,6 +27,8 @@ import GHC.Generics (Generic)
 import Text.Read (Read)
 import Text.Show (Show(show))
 
+import Data.CaseInsensitive (CI)
+import qualified Data.CaseInsensitive as CI (mk)
 import Data.Text (Text)
 import qualified Data.Text as Text (pack, unpack)
 import Language.Haskell.TH.Syntax (Lift(lift))
@@ -53,14 +55,15 @@ data LogLevel
     -- ^ General information messages.
     | LevelDebug
     -- ^ Messges useful for debugging.
-    | LevelOther Text
+    | LevelOther !(CI Text)
     -- ^ User defined logging level.
   deriving (Data, Eq, Generic, Ord, Read, Show, Typeable)
 
-toText :: LogLevel -> Text
+-- | Convert (syslog) 'LogLevel' into (case insensitive) 'Text'.
+toText :: LogLevel -> CI Text
 toText = \case
     LevelOther t -> t
-    level        -> Text.pack . List.drop 5 $ show level
+    level        -> CI.mk . Text.pack . List.drop 5 $ show level
 {-# INLINEABLE toText #-}
 
 instance ToLogStr LogLevel where
@@ -77,4 +80,5 @@ instance Lift LogLevel where
         LevelNotice    -> [|LevelNotice|]
         LevelInfo      -> [|LevelInfo|]
         LevelDebug     -> [|LevelDebug|]
-        LevelOther x   -> [|LevelOther $ Text.pack $(lift $ Text.unpack x)|]
+        LevelOther x   ->
+            [|LevelOther . CI.mk $ Text.pack $(lift $ Text.unpack x)|]
